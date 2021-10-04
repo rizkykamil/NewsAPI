cf1b275ecb20cd880fff12617b54693e72bd34ac
rizkykamil.dll <rabbaniikamil@gmail.com>
2021-10-04 08:24:14 +0700

    Initial Commit

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by Rizky Kamil Rabbani on 04/10/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
